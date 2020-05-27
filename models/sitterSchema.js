489c7409353de68652a124c023935461c704f5ae
Anakonda11 <Xapollon430@gmail.com>
2020-05-26 22:51:18 -0400

    sitter schema added

---
+++ models/sitterSchema.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+const sitterSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    // validate(email) {
+    //   if (!Validator.isEmail(email)) {
+    //     throw new Error("Please enter a valid email");
+    //   }
+    // },
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+  },
+  adress: {
+    street: String,
+    city: String,
+    zip: Number,
+    location: {
+      latitude: String,
+      longitude: String,
+    },
+  },
+  phoneNumber: String,
+  rate: Number,
+  //   profileComments
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: "10000",
+  });
+  return token;
+};
+
+let Sitter = mongoose.model("Sitter", sitterSchema);
+
+module.exports = {
+  Sitter,
+};
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const Route = require("./routes/route");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+const app = express();
 
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +18,5 @@ app.use(Route);
 // app.use(express.static("static"));
 
 app.listen(PORT, () => {
-	console.log("Listening");
+  console.log("Listening");
 });
