b3815cdb9f56b39e1af35498d2979156f80ab694
Anakonda11 <Xapollon430@gmail.com>
2020-06-11 11:30:48 -0400

    cra added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const Routes = require("./routes/route");
 const mongoose = require("mongoose");
+const path = require("path");
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
@@ -14,7 +16,13 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.use(express.json());
 app.use(cors());
-app.use(Routes);
+app.use("/auth", Routes);
+
+app.use(express.static("./src/client/build"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.use(async (req, res, next) => {
   return res.status(req.error.code).json({ error: req.error.message });
