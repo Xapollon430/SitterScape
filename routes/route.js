e67d8c42895dfd3e1de41b0b1ad6923253aabba2
Anakonda11 <Xapollon430@gmail.com>
2020-06-12 01:37:08 -0400

    testing additional routes

--- routes/route.js
+++ routes/route.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/controller");
 const HttpError = require("../error/HttpError");
+const routetest = require("./routetest");
 
 router.post("/sign-up", Controller.signUp);
 
 router.post("/login", Controller.login);
+
+router.use("/test", routetest);
 
 router.use((req, res) => {
   return res.json({ error: "Route doesnt exist" });
