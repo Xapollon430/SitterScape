4e1390a9bb718d27bd93c20ef0581c7567a0cc18
Anakonda11 <Xapollon430@gmail.com>
2020-06-18 03:18:40 -0400

    owner vs sitter commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const Routes = require("./routes/route");
 const mongoose = require("mongoose");
-const path = require("path");
 
 require("dotenv").config();
 
