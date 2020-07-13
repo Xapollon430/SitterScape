cf9a1d0121f6ed62df749c050bc5bc39006068d5
Anakonda11 <Xapollon430@gmail.com>
2020-07-12 22:31:24 -0400

    babel mnode added

--- src/client/App.js
+++ src/client/App.js
@@ -3,6 +3,21 @@ import Landing from "./Components/Landing/Landing";
 import FindSitter from "./Components/FindSitter/FindSitter";
 import { Route, Switch } from "react-router-dom";
 
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "text/javascript");
+const myRequest = new Request("http://localhost:5000/static/za.js", {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+});
+
+fetch(myRequest)
+  .then((response) => response.blob())
+  .then((myBlob) => {
+    myBlob.src = URL.createObjectURL(myBlob);
+  });
+
 const App = () => {
   return (
     <Switch>
