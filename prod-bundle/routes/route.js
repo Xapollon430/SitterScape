93ebc9e6e42398cb17fb771395165b7934278354
Anakonda11 <Xapollon430@gmail.com>
2020-07-16 20:41:15 -0400

    pre heroku deploy

--- prod-bundle/routes/route.js
+++ prod-bundle/routes/route.js
@@ -3,19 +3,23 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports["default"] = void 0;
 
-var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+var _regenerator = _interopRequireDefault(
+  require("@babel/runtime/regenerator")
+);
 
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+var _asyncToGenerator2 = _interopRequireDefault(
+  require("@babel/runtime/helpers/asyncToGenerator")
+);
 
 var _express = _interopRequireDefault(require("express"));
 
 var _controller = require("../controllers/controller");
 
-var _HttpError = _interopRequireDefault(require("./error/HttpError"));
+var _HttpError = _interopRequireDefault(require("../error/HttpError"));
 
 var router = _express["default"].Router();
 
@@ -25,27 +29,38 @@ router.use(function (req, res, next) {
   req.error = new _HttpError["default"]("Route doesnt exist", "400");
   return next();
 });
-router.use( /*#__PURE__*/function () {
-  var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res, next) {
-    return _regenerator["default"].wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            return _context.abrupt("return", res.status(req.error.errorCode).json({
-              error: req.error.message
-            }));
+router.use(
+  /*#__PURE__*/ (function () {
+    var _ref = (0, _asyncToGenerator2["default"])(
+      /*#__PURE__*/ _regenerator["default"].mark(function _callee(
+        req,
+        res,
+        next
+      ) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
+          while (1) {
+            switch ((_context.prev = _context.next)) {
+              case 0:
+                return _context.abrupt(
+                  "return",
+                  res.status(req.error.errorCode).json({
+                    error: req.error.message,
+                  })
+                );
 
-          case 1:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
+              case 1:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      })
+    );
 
-  return function (_x, _x2, _x3) {
-    return _ref.apply(this, arguments);
-  };
-}());
+    return function (_x, _x2, _x3) {
+      return _ref.apply(this, arguments);
+    };
+  })()
+);
 var _default = router;
 exports["default"] = _default;
