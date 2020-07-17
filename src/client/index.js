ef0806b1e1b750989b35885b993e46be40a7e361
Anakonda11 <Xapollon430@gmail.com>
2020-07-17 13:35:24 -0400

    fixed webpack bug

--- src/client/index.js
+++ src/client/index.js
@@ -5,8 +5,6 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/store";
 
-console.log(process.env.SIT_API_URL);
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+//This is for client side code, the server doesnt use webpack and only uses babel
+
+module.exports = (env) => {
+  return {
+    entry: "./src/client/index.js",
+    output: {
+      path: path.resolve(__dirname, "prod-bundle/static"),
+      filename: "react-bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+
+    devServer: {
+      historyApiFallback: true,
+      open: true,
+      port: 3000,
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: "./src/server/static/index.html",
+      }),
+
+      new webpack.DefinePlugin({
+        "process.env.SIT_API_URL":
+          env.ENV === "production"
+            ? JSON.stringify("https://vk-sit.herokuapp.com")
+            : JSON.stringify("http://localhost:5000"),
+      }),
+    ],
+  };
+};
