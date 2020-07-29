b3557f146c9b56bbb373d41a2602bfa58b4cd857
Anakonda11 <Xapollon430@gmail.com>
2020-07-29 14:19:31 -0400

    bg image fixed

--- src/client/Components/Landing/LandingCss.js
+++ src/client/Components/Landing/LandingCss.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 let BackgroundImage = styled.div`
   min-width: 334px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   background-image: url("${process.env.SIT_API_URL}/static/images/background.jpg");
   height: 100vh;
   width: 100vw;
