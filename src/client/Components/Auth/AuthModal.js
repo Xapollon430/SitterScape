ad8d8eb4e650922848ab82bee6f385c88d1de7fa
Anakonda11 <Xapollon430@gmail.com>
2020-10-20 23:43:06 -0400

    refactoring auth

--- src/client/Components/Auth/AuthModal.js
+++ src/client/Components/Auth/AuthModal.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import Login from "./Login/Login";
 import SignUp from "./SignUp/SignUp";
-import AuthTabs from "./AuthTabs/AuthTabs";
-import { CancelIcon } from "./AuthModalCss";
+import { CancelIcon, Spinner } from "./AuthModalCss";
 import { useDispatch, useSelector } from "react-redux";
 import {
   changeIsLogInOpen,
@@ -34,8 +33,9 @@ const AuthModal = ({ onClose }) => {
   return (
     <React.Fragment>
       <CancelIcon onClick={onClose} className="fas fa-times fa-2x" />
-      <AuthTabs changeTab={changeTab} />
-      {state.modalState.isLogInOpen ? <Login /> : <SignUp />}
+      {/* <AuthTabs changeTab={changeTab} /> */}
+      <Spinner />
+      {/* {state.modalState.isLogInOpen ? <Login /> : <SignUp />} */}
     </React.Fragment>
   );
 };
--- src/client/Components/Auth/AuthModalCss.js
+++ src/client/Components/Auth/AuthModalCss.js
@@ -1,8 +1,26 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const CancelIcon = styled.i`
   color: rgb(20, 169, 109);
   position: absolute;
   right: 10px;
   top: 5px;
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Spinner = styled.div`
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid rgb(20, 169, 109);
+  width: 120px;
+  height: 120px;
+  animation: ${spin} 2s linear infinite;
 `;
