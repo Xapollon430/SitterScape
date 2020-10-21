982e631769ff74a8b679a3007a679b8bea822154
Anakonda11 <Xapollon430@gmail.com>
2020-10-20 22:02:31 -0400

    refactoring auth

--- src/client/Components/Auth/AuthModal.js
+++ src/client/Components/Auth/AuthModal.js
@@ -7,86 +7,35 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   changeIsLogInOpen,
   changeIsSignUpOpen,
-  changeIsModalOpen,
 } from "../../store/actions/AuthModalActions";
-import { changeLoggedIn, logUserIn } from "../../store/actions/GeneralActions";
-import { signUpFormChecker, logInFormChecker } from "./AuthHelper";
 import { Post } from "../../Functions/Functions";
 
 const AuthModal = ({ onClose }) => {
-  const [userInfo, setUserInfo] = useState({});
-  const [formError, setFormError] = useState({});
-  const [errorMessageFromServer, setErrorMessageFromServer] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const submitHandler = async (e, type) => {
-    e.preventDefault();
-
-    let errors =
-      type === "login"
-        ? logInFormChecker(userInfo)
-        : signUpFormChecker(userInfo);
-
-    if (errors.errorExists) {
-      setFormError(errors);
-    } else {
-      setIsLoading(true);
-      let { user, token, error } = await Post(
-        `${process.env.SIT_API_URL}/api/${type}`,
-        userInfo
-      );
-      if (user && token) {
-        localStorage.setItem("jwt-token", data.token);
-        dispatch(logUserIn(data.user));
-        dispatch(changeLoggedIn(true));
-        dispatch(changeIsModalOpen(false));
-      } else {
-        setErrorMessageFromServer(data.error);
-        setUserInfo({});
-      }
-    }
-  };
-
-  const onChange = (e) => {
-    setUserInfo({
-      ...userInfo,
-      [e.target.getAttribute("name")]: e.target.value,
-    });
-  };
+  // let { user, token, error } = await Post(
+  //   `${process.env.SIT_API_URL}/api/${type}`,
+  //   userInfo
+  // );
+  // if (user && token) {
+  //   localStorage.setItem("jwt-token", data.token);
+  //   dispatch(logUserIn(data.user));
+  //   dispatch(changeLoggedIn(true));
+  //   dispatch(changeIsModalOpen(false));
+  // }
 
   const changeTab = (e) => {
     e.target.getAttribute("name") === "login"
       ? dispatch(changeIsLogInOpen(true))
       : dispatch(changeIsSignUpOpen(true));
-    setFormError({});
-    setUserInfo({});
-    setErrorMessageFromServer(null);
   };
 
   return (
     <React.Fragment>
       <CancelIcon onClick={onClose} className="fas fa-times fa-2x" />
-      <AuthTabs
-        changeTab={changeTab}
-        errorMessageFromServer={errorMessageFromServer}
-      />
-      {state.modalState.isLogInOpen ? (
-        <Login
-          onChange={onChange}
-          submitHandler={submitHandler}
-          formError={formError}
-          isLoading={isLoading}
-        />
-      ) : (
-        <SignUp
-          onChange={onChange}
-          submitHandler={submitHandler}
-          formError={formError}
-          isLoading={isLoading}
-        />
-      )}
+      <AuthTabs changeTab={changeTab} />
+      {state.modalState.isLogInOpen ? <Login /> : <SignUp />}
     </React.Fragment>
   );
 };
