6b313231e92f1e10d5026b4c2c7f20b28aa67185
Anakonda11 <Xapollon430@gmail.com>
2020-11-24 23:21:27 -0500

    leaflet added wow

--- src/client/Components/FindSitter/FindSitter.js
+++ src/client/Components/FindSitter/FindSitter.js
@@ -4,19 +4,19 @@ import styled from "styled-components";
 
 const LMap = styled.div`
   height: 180px;
-  width: 500px;
+  max-width: 600px;
+  min-width: 200px;
+  width: 50vw;
 `;
 
 const FindSitter = () => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    var mymap = L.map(mapRef.current).setView([51.505, -0.09], 13);
-
-    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(mymap);
+    const mymap = L.map(mapRef.current).setView([51.505, -0.09], 13);
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+    ).addTo(mymap);
   }, []);
 
   return (
