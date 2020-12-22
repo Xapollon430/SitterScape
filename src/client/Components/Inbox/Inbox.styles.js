47b2b08198d8ea5439624832c99a7dc24e12ddfb
Xapollon430 <Xapollon430@gmail.com>
2020-12-22 18:26:57 -0500

    package.json fixed

--- src/client/Components/Inbox/Inbox.styles.js
+++ src/client/Components/Inbox/Inbox.styles.js
@@ -2,13 +2,12 @@ import styled from "styled-components";
 
 export const InboxWrap = styled.div`
   height: 100vh;
-  min-width: 340px;
+  /* min-width: 340px; */
   display: grid;
   grid-template-rows: 60px 1fr;
 `;
 
 export const Inbox = styled.div`
-  width: 100%;
   height: 100%;
   background-color: red;
 `;
