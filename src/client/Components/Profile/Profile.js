c8d50c755335f020806fd5c03d5a41e7bb260a1a
Xapollon430 <Xapollon430@gmail.com>
2021-02-17 20:36:52 -0500

    profile setup

---
+++ src/client/Components/Profile/Profile.js
@@ -0,0 +1,7 @@
+import { TextField } from "@material-ui/core";
+
+const Profile = () => {
+  return <TextField id="outlined-basic" label="Your Name" variant="outlined" />;
+};
+
+export default Profile;
