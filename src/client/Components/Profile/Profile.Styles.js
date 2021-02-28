225f04d1d002ededaf58b1e8c2f87d79843f85f5
Xapollon430 <Xapollon430@gmail.com>
2021-02-28 14:55:59 -0500

    profile cleanup

--- src/client/Components/Profile/Profile.Styles.js
+++ src/client/Components/Profile/Profile.Styles.js
@@ -1,9 +1,11 @@
+import { TextField } from "@material-ui/core";
 import styled from "styled-components";
 
 export const ProfileWrap = styled.div`
   width: 100%;
-  height: 1000px;
-  margin: 100px auto 0px auto;
+  margin: 0px auto;
+  padding: 120px 0px 100px 0px;
+  background-color: #eaebed;
 `;
 
 export const FormWrap = styled.div`
@@ -12,6 +14,10 @@ export const FormWrap = styled.div`
   margin: 0px auto;
   padding: 20px;
   box-sizing: border-box;
+  display: grid;
+  grid-gap: 30px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  background-color: white;
 `;
 
 export const PersonalInfoWrap = styled.div`
@@ -19,9 +25,53 @@ export const PersonalInfoWrap = styled.div`
   grid-template-rows: 40px 50px 50px 50px;
   grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
+
+  @media (max-width: 600px) {
+    grid-template-rows: repeat(6, 50px);
+    grid-template-columns: 1fr;
+  }
 `;
 
-export const PersonalInfoText = styled.h3`
-  grid-column: span 2;
+export const InfoText = styled.h3`
+  grid-column: 1/-1;
   align-self: center;
+`;
+
+export const AdressWrap = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 40px 50px 50px;
+  grid-gap: 20px;
+`;
+
+export const AdressField = styled(TextField)`
+  grid-column: 1/-1;
+`;
+
+export const PhotoWrap = styled.div`
+  display: grid;
+  grid-template-columns: 5fr 2fr;
+  justify-items: center;
+  @media (max-width: 600px) {
+    grid-template-rows: auto auto;
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const PhotoTextExplanation = styled.div`
+  color: #66727c;
+`;
+
+export const UserImage = styled.img`
+  width: 200px;
+  @media (max-width: 600px) {
+    margin-top: 15px;
+    max-width: 150px;
+  }
+`;
+
+export const PhotoTextWrap = styled.div``;
+
+export const UploadButton = styled.input`
+  margin-top: 20px;
 `;
