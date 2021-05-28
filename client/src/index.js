4245532f944a19c5ccef02977ecfd25a6a9a5950
Vehbi <xapollon430@gmail.com>
2021-05-27 22:45:36 -0400

    test

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Store from "./store/store";
+import "./index.css";
 
 ReactDOM.render(
   <Store>
