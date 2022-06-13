573c4de92460bd885455047625413b77bdcf645d
Vehbi <xapollon430@gmail.com>
2022-06-13 04:40:11 -0400

    123

--- client/src/components/SearchSitter/SearchSitter.js
+++ client/src/components/SearchSitter/SearchSitter.js
@@ -255,7 +255,6 @@ const SearchSitter = () => {
                   { ...values, address: `${center.lat},${center.lng}` },
                   bounds
                 );
-                console.log("inside");
               }
 
               prevZoom = zoom;
