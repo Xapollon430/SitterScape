4c5a36909169d947bd93ec9b33f05620b4fd55f9
Vehbi <xapollon430@gmail.com>
2022-06-16 00:21:11 -0400

    123

--- client/src/components/SearchSitter/SearchSitter.styles.js
+++ client/src/components/SearchSitter/SearchSitter.styles.js
@@ -176,7 +176,7 @@ export const FilterMapToggleButton = styled.div`
   display: grid;
   grid-template-columns: 100px 100px;
   left: 50%;
-  bottom: 150px;
+  bottom: 100px;
   transform: translate(-50%, -50%);
   z-index: 1010;
 
